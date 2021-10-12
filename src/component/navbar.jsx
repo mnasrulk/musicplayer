a895aadfa0f6a9e121ab73829242ff97adb6f2a0
mnasrulk <m.nasrul.k@gmail.com>
2021-10-12 15:22:04 +0700

    commit on 'master'

---
+++ src/component/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Navbar extends Component{
+    navbarStyle = {
+        margin: "auto",
+    };
+    render(){
+        return(
+            <div className="navbar" style={this.navbarStyle}>
+                <h3 style={{margin:"10px"}}>
+                    <span role="img">📻</span>
+                    WebPod
+                </h3>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
---
+++ src/component/player.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import VideoDetail from "./VideoDetail";
+import _ from "lodash";
+
+import "./player.css";
+
+class Player extends Component{
+    state = {};
+    render(){
+        const { video, onVideoEvent } = this.props;
+        const {
+            channelId,
+            publishedAt,
+            title,
+            description,
+            thumbnails,
+            channelTitle,
+        }= !_.isEmpty(video) ? video.snippet : {};
+        const imageURl = !_.isEmpty(video) ? thumbnails.medium.url : "";
+        const publishedDate = !_.isEmpty(video) ? publishedAt.split("T")[0] : "";
+        return(
+            <div className="Player">
+                <div className="Background"></div>
+                    <div className="Header">
+                        <div className="Title">Now Playing</div>
+                    </div>
+                    <div className="Artwork">
+                        <img src={imageURl}/>
+                    </div>
+                    <div className="TrackInformation">
+                        <div className="Name"></div>
+                        <div className="Artist">{title}</div>
+                        <div className="Album">{description}</div>
+                    </div>
+                    <div className="Scrubber">
+                        <div className="Scrubber-Progress"></div>
+                    </div>
+                    <div classname="Controls">
+                        <div className="Button">
+                            <i className="fab fa-fw fa-play"></i>
+                            <img src="assets/mchammer.gif" className="hammertime"></img>
+                        </div>
+                    </div>
+                    <div className="Timestamps">
+                        <div className="Time Time--current">{publishedDate}</div>
+                        <div className="Time Time--total">{channelTitle}</div>
+                    </div>
+                    {!_.isEmpty(video) && (
+                        <VideoDetail video={video} onVideoEvent={(c) => onVideoEvent(c)}/>
+                    )}
+            </div>
+        );
+    }
+}
